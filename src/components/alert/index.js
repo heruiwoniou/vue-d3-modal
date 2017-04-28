d1ed29beb409e0fa18145c90fa1bf1edac016529
heruiwoniou <heruiwoniou@qq.com>
2017-04-28 12:41:56 +0800

    '修改了移动过程中svg的呈现过程'

--- src/components/alert/index.js
+++ src/components/alert/index.js
@@ -1,22 +1,7 @@
 import alertbase from './alert.vue';
 let alertConstructor = Vue.extend(alertbase);
 let alerts = {};
-/**
- * create modal
- * @function modal
- * @memberof WebApi
- * @param {Object} options - 弹框参数对象
- * @param {string} options.name - 窗体名称
- * @param {string} options.title - 标题
- * @param {number} options.width - 宽度
- * @param {number} options.height - 高
- * @param {number} options.titleHeight - 标题高
- * @param {number} options.borderRadius - 圆角
- * @param {number} options.closeIconSize - 关闭按钮尺寸
- * @param {string} options.background - 背景
- * @param {string} options.src - 地址
- * @return {promise} promise - promise.then(resolve,reject)
- */
+
 function Alert(options = {}) {
     let instance;
     let defaultOptions = _.assign({
